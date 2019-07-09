be8a5f05d9ed5d6318370ce947e73cb25295b5dd
Judah Jacobson <judah@users.noreply.github.com>
2019-07-09 15:56:07 -0700

    Run check.sh in presubmit.

--- kokoro/gcp_ubuntu/kokoro_build.sh
+++ kokoro/gcp_ubuntu/kokoro_build.sh
@@ -4,4 +4,11 @@ set -xeuo pipefail
 
 cd "${KOKORO_ARTIFACTS_DIR}"/github/ghc-source-gen
 
-ls
+STACK_URL=https://github.com/commercialhaskell/stack/releases/download/v2.1.1/stack-2.1.1-linux-x86_64.tar.gz
+
+mkdir -p "$HOME"/.local/bin
+export PATH="$HOME/.local/bin:$PATH"
+
+curl -L "$STACK_URL" | tar xz --wildcards --strip-components=1 -C $HOME/.local/bin '*/stack'
+
+./check.sh
