08fe48b3c2ee20a226e8528fa41b0b4808970f67
Greg Steuck <greg@nest.cx>
2022-08-21 14:30:47 -0700

    Pin newer versions of stackage, update CI versions

--- .circleci/install-stack.sh
+++ .circleci/install-stack.sh
@@ -2,7 +2,7 @@
 
 set -xueo pipefail
 mkdir -p $HOME/.local/bin
-curl -L https://github.com/commercialhaskell/stack/releases/download/v2.1.1/stack-2.1.1-linux-x86_64.tar.gz \
+curl -L https://github.com/commercialhaskell/stack/releases/download/v2.7.5/stack-2.7.5-linux-x86_64.tar.gz \
     | tar xz --wildcards --strip-components=1 -C "$HOME/.local/bin" '*/stack'
 echo 'export PATH=$HOME/.local/bin:$PATH' >> $BASH_ENV
 
