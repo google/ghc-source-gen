google/ghc-source-gen

commit 26256ea4edcce41adbf46f2767e7ff4e74979626

Support GHC 9.8 (#108) (#112)
* Support GHC 9.8

* Update ghc-source-gen.cabal

* Add 9.8 GHC CI target

ghc-source-gen.cabal
@@ -61,7 +61,7 @@ library
       TypeSynonymInstances
   build-depends:
       base >=4.7 && <5
-    , ghc >=8.4 && <9.7
+    , ghc >=8.4 && <9.9
   default-language: Haskell2010
   if impl(ghc<8.10)
     other-modules:
@@ -104,7 +104,7 @@ test-suite name_test
   build-depends:
       QuickCheck >=2.10 && <2.15
     , base >=4.7 && <5
-    , ghc >=8.4 && <9.7
+    , ghc >=8.4 && <9.9
     , ghc-source-gen
     , tasty >=1.0 && <1.5
     , tasty-hunit ==0.10.*
@@ -125,7 +125,7 @@ test-suite pprint_examples
       TypeSynonymInstances
   build-depends:
       base >=4.7 && <5
-    , ghc >=8.4 && <9.7
+    , ghc >=8.4 && <9.9
     , ghc-paths ==0.1.*
     , ghc-source-gen
     , tasty >=1.0 && <1.5
@@ -151,7 +151,7 @@ test-suite pprint_test
       TypeSynonymInstances
   build-depends:
       base >=4.7 && <5
-    , ghc >=8.4 && <9.7
+    , ghc >=8.4 && <9.9
     , ghc-paths ==0.1.*
     , ghc-source-gen
     , tasty >=1.0 && <1.5

src/GHC/SourceGen/Module.hs
@@ -177,7 +177,7 @@ source d = d { ideclSource =
 -- > =====
 -- > thingAll "A"
 thingAll :: RdrNameStr -> IE'
-thingAll = withEpAnnNotUsed IEThingAll . wrappedName
+thingAll = withEpAnnNotUsed' IEThingAll . wrappedName
 
 -- | Exports specific methods and/or constructors.
 --
@@ -185,7 +185,7 @@ thingAll = withEpAnnNotUsed IEThingAll . wrappedName
 -- > =====
 -- > thingWith "A" ["b", "C"]
 thingWith :: RdrNameStr -> [OccNameStr] -> IE'
-thingWith n cs = withEpAnnNotUsed IEThingWith (wrappedName n) NoIEWildcard
+thingWith n cs = withEpAnnNotUsed' IEThingWith (wrappedName n) NoIEWildcard
                     (map (wrappedName . unqual) cs)
 #if !MIN_VERSION_ghc(9,2,0)
                     -- The parsing step leaves the list of fields empty
@@ -212,4 +212,12 @@ wrappedName = mkLocated . IEName . exportRdrName
 -- > =====
 -- > moduleContents "M"
 moduleContents :: ModuleNameStr -> IE'
-moduleContents = withEpAnnNotUsed IEModuleContents . mkLocated . unModuleNameStr
+moduleContents = withEpAnnNotUsed' IEModuleContents . mkLocated . unModuleNameStr
+
+
+#if MIN_VERSION_ghc(9,8,0)
+withEpAnnNotUsed' :: ((Maybe x, EpAnn ann) -> a) -> a
+withEpAnnNotUsed' = ($ (Nothing, EpAnnNotUsed))
+#else
+withEpAnnNotUsed' = withEpAnnNotUsed
+#endif

.github/workflows/ci.yaml
@@ -9,7 +9,7 @@ jobs:
       fail-fast: false
       matrix:
         os: [ubuntu-latest]
-        ghc: ["9.0", "9.2", "9.4", "9.6"]
+        ghc: ["9.0", "9.2", "9.4", "9.6", "9.8"]
     env:
       STACK_YAML: stack-${{ matrix.ghc }}.yaml
     runs-on: ${{ matrix.os }}

.gitignore
@@ -2,3 +2,4 @@
 dist/
 # Generated automatically by hpack:
 *.yaml.lock
+dist-newstyle

src/GHC/SourceGen/Binds/Internal.hs
@@ -8,7 +8,11 @@
 module GHC.SourceGen.Binds.Internal where
 
 #if MIN_VERSION_ghc(9,0,0)
-import GHC.Types.Basic (Origin(Generated))
+import GHC.Types.Basic ( Origin(Generated)
+#if MIN_VERSION_ghc(9,8,0)
+                       , DoPmc (DoPmc)
+#endif
+                       )
 import GHC.Data.Bag (listToBag)
 #else
 import BasicTypes (Origin(Generated))
@@ -83,7 +87,9 @@ data RawGRHSs = RawGRHSs
 
 matchGroup :: HsMatchContext' -> [RawMatch] -> MatchGroup' LHsExpr'
 matchGroup context matches =
-#if MIN_VERSION_ghc(9,6,0)
+#if MIN_VERSION_ghc(9,8,0)
+    MG (Generated DoPmc)
+#elif MIN_VERSION_ghc(9,6,0)
     MG Generated
 #else
     noExt MG

src/GHC/SourceGen/Expr.hs
@@ -294,7 +294,9 @@ recordUpd e fs =
             }
     withPlaceHolder4 = withPlaceHolder . withPlaceHolder . withPlaceHolder
                             . withPlaceHolder
-#if MIN_VERSION_ghc(9,2,0)
+#if MIN_VERSION_ghc(9,8,0)
+    toRecordUpdFields = noExt RegularRecUpdFields
+#elif MIN_VERSION_ghc(9,2,0)
     toRecordUpdFields = Left
 #else
     toRecordUpdFields = id

src/GHC/SourceGen/Overloaded.hs
@@ -21,6 +21,9 @@ module GHC.SourceGen.Overloaded
 import GHC.Hs.Type
     ( HsType(..)
     , HsTyVarBndr(..)
+#if MIN_VERSION_ghc(9,8,0)
+    , HsBndrVis (HsBndrRequired)
+#endif
     )
 import GHC.Hs (IE(..), IEWrappedName(..)
 #if MIN_VERSION_ghc(9,6,0)
@@ -280,8 +283,11 @@ instance BVar HsType' where
 
 #if MIN_VERSION_ghc(9,0,0)
 instance BVar HsTyVarBndr' where
+#if MIN_VERSION_ghc(9,8,0)
+    bvar = withEpAnnNotUsed UserTyVar HsBndrRequired . typeRdrName . UnqualStr
+#else
     bvar = withEpAnnNotUsed UserTyVar () . typeRdrName . UnqualStr
-
+#endif
 instance BVar HsTyVarBndrS' where
     bvar = withEpAnnNotUsed UserTyVar SpecifiedSpec . typeRdrName . UnqualStr
 #else
@@ -290,14 +296,21 @@ instance BVar HsTyVarBndr' where
 #endif
 
 instance Var IE' where
-    var n =
-      noExt IEVar $ mkLocated $
-#if MIN_VERSION_ghc(9,6,0)
-      (IEName noExtField)
+    var n = ie_var $ mkLocated $ ie_name $ exportRdrName n
+      where
+        ie_var =
+#if MIN_VERSION_ghc(9,8,0)
+          IEVar Nothing
 #else
-      IEName
+          noExt IEVar
 #endif
-      $ exportRdrName n
+
+        ie_name = 
+#if MIN_VERSION_ghc(9,6,0)
+          noExt IEName
+#else
+          IEName
+#endif
 
 instance BVar IE' where
     bvar = var . UnqualStr

src/GHC/SourceGen/Syntax/Internal.hs
@@ -80,6 +80,10 @@ import BasicTypes (PromotionFlag(..))
 import GHC.Hs.Type (Promoted(..))
 #endif
 
+#if MIN_VERSION_ghc(9,8,0)
+import GHC.Hs.Type (HsBndrVis)
+#endif
+
 #if MIN_VERSION_ghc(8,10,0)
 import qualified GHC.Hs as GHC
 #elif MIN_VERSION_ghc(8,6,0)
@@ -281,7 +285,10 @@ type IE' = IE GhcPs
 -- Instances:
 --
 -- * 'GHC.SourceGen.Overloaded.BVar'
-#if MIN_VERSION_ghc(9,0,0)
+#if MIN_VERSION_ghc(9,8,0)
+type HsTyVarBndr' = HsTyVarBndr (HsBndrVis GhcPs) GhcPs
+type HsTyVarBndrS' = HsTyVarBndr Specificity GhcPs
+#elif MIN_VERSION_ghc(9,0,0)
 type HsTyVarBndr' = HsTyVarBndr () GhcPs
 type HsTyVarBndrS' = HsTyVarBndr Specificity GhcPs
 #else
@@ -323,7 +330,11 @@ type LHsSigWcType' = LHsSigWcType GhcPs
 type LHsWcType' = LHsWcType GhcPs
 type HsDerivingClause' = HsDerivingClause GhcPs
 type LHsRecField' arg = LHsRecField GhcPs arg
+#if MIN_VERSION_ghc(9,8,0)
+type LHsRecUpdField' = LHsRecUpdField GhcPs GhcPs
+#else
 type LHsRecUpdField' = LHsRecUpdField GhcPs
+#endif
 type LPat' = LPat GhcPs
 type TyFamInstDecl' = TyFamInstDecl GhcPs
 

commit d9707003aa9e922afc08c4f2b19d550c8dd344d3

Fix #112 with support for GHC 9.8 (#113)
* Add missing signature for overloaded `withEpAnnNotUsed`

* Fixed issued caused by #112

* Stack support for GHC 9.8

* Add stack-9.8.yaml

* Bump Tasty to `< 1.6`

ghc-source-gen.cabal
@@ -106,7 +106,7 @@ test-suite name_test
     , base >=4.7 && <5
     , ghc >=8.4 && <9.9
     , ghc-source-gen
-    , tasty >=1.0 && <1.5
+    , tasty >=1.0 && <1.6
     , tasty-hunit ==0.10.*
     , tasty-quickcheck >=0.9 && <0.11
   default-language: Haskell2010
@@ -128,7 +128,7 @@ test-suite pprint_examples
     , ghc >=8.4 && <9.9
     , ghc-paths ==0.1.*
     , ghc-source-gen
-    , tasty >=1.0 && <1.5
+    , tasty >=1.0 && <1.6
     , tasty-hunit ==0.10.*
   default-language: Haskell2010
   if impl(ghc<9.0)
@@ -154,7 +154,7 @@ test-suite pprint_test
     , ghc >=8.4 && <9.9
     , ghc-paths ==0.1.*
     , ghc-source-gen
-    , tasty >=1.0 && <1.5
+    , tasty >=1.0 && <1.6
     , tasty-hunit ==0.10.*
   default-language: Haskell2010
   if impl(ghc<9.0)

package.yaml
@@ -30,7 +30,7 @@ description: |
 
 dependencies:
 - base >= 4.7 && < 5
-- ghc >= 8.4 && < 9.7
+- ghc >= 8.4 && < 9.9
 
 default-extensions:
 - DataKinds
@@ -84,7 +84,7 @@ tests:
     dependencies:
     - ghc-source-gen
     - ghc-paths == 0.1.*
-    - tasty >= 1.0 && < 1.5
+    - tasty >= 1.0 && < 1.6
     - tasty-hunit == 0.10.*
 
     when:
@@ -100,7 +100,7 @@ tests:
     dependencies:
     - ghc-source-gen
     - ghc-paths == 0.1.*
-    - tasty >= 1.0 && < 1.5
+    - tasty >= 1.0 && < 1.6
     - tasty-hunit == 0.10.*
 
     when:
@@ -117,6 +117,6 @@ tests:
     dependencies:
     - ghc-source-gen
     - QuickCheck >= 2.10 && < 2.15
-    - tasty >= 1.0 && < 1.5
+    - tasty >= 1.0 && < 1.6
     - tasty-hunit == 0.10.*
     - tasty-quickcheck >= 0.9 && < 0.11

src/GHC/SourceGen/Module.hs
@@ -219,5 +219,14 @@ moduleContents = withEpAnnNotUsed' IEModuleContents . mkLocated . unModuleNameSt
 withEpAnnNotUsed' :: ((Maybe x, EpAnn ann) -> a) -> a
 withEpAnnNotUsed' = ($ (Nothing, EpAnnNotUsed))
 #else
+
+#if MIN_VERSION_ghc(9,2,0)
+withEpAnnNotUsed' :: (EpAnn ann -> a) -> a
+#elif MIN_VERSION_ghc(8,6,0)
+withEpAnnNotUsed' :: (NoExtField -> a) -> a
+#else
+withEpAnnNotUsed' :: a -> a
+#endif
 withEpAnnNotUsed' = withEpAnnNotUsed
+
 #endif

stack-9.8.yaml
@@ -1,0 +1,15 @@
+# Copyright 2019 Google LLC
+#
+# Use of this source code is governed by a BSD-style
+# license that can be found in the LICENSE file or at
+# https://developers.google.com/open-source/licenses/bsd
+
+resolver: nightly-2024-03-29
+compiler: ghc-9.8.2
+
+packages:
+- .
+- ghc-show-ast
+
+ghc-options:
+  "$locals": -Wall -Werror -Wwarn=unused-imports -Wwarn=dodgy-imports